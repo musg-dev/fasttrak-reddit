88cb49efc8102d6db4c8b367e073fc234c5701fc
Chance Callahan <chance@chancecallahan.com>
2021-05-09 12:37:59 -0400

    Bot is functional as it was before refactor, but now with error reporting.

--- core/reddit/bot_parser.py
+++ core/reddit/bot_parser.py
@@ -1,16 +1,22 @@
-import re
+import js_regex as re
 
-def parse_vote(x):
-	if re.match(x, "/(Aye)/i") == True:
-		print("Core::Reddit::Parser::Vote // [AYE] " + i.author.name)
-		return 1
-	if re.match(x, "/(Yea)/i") == True:
-		print("Core::Reddit::Parser::Vote // [AYE] " + i.author.name)
-		return 1
-	if re.match(x, "/(Nay)/i") == True:
-		print("Core::Reddit::Parser::Vote // [NAY] " + i.author.name)
-		return 0
-	if re.match(x, "/(Abstain)/i") == True:
-		print("Core::Reddit::Parser::Vote // [ABSTAIN] " + i.author.name)
-		return 2
 
+def parse_vote(x, y):
+    if re.compile("(Aye)").search(x):
+        print("Core::Reddit::Parser::Vote // [AYE] " + y)
+        return 1
+    if re.compile("(Yea)").search(x):
+        print("Core::Reddit::Parser::Vote // [AYE] " + y)
+        return 1
+    if re.compile("(Yay)").search(x):
+        print("Core::Reddit::Parser::Vote // [AYE] " + y)
+        return 1
+    if re.compile("(Nay)").search(x):
+        print("Core::Reddit::Parser::Vote // [NAY] " + y)
+        return 0
+    if re.compile("(Abstain)").search(x):
+        print("Core::Reddit::Parser::Vote // [ABSTAIN] " + y)
+        return 2
+    if re.compile("(FastTRAK)").search(x):
+        print("Core::Reddit::Parser::Vote // [AYE] " + y)
+        return 10
--- core/reddit/tabulation.py
+++ core/reddit/tabulation.py
@@ -1,46 +1,46 @@
+import emoji
+
 import reddit
 from sentry_sdk import capture_message
 import praw
 import datetime
 import reddit
 
-# Variables
 
-aye = 0
-nay = 0
-abst = 0
-
-
-def clear_vars():
+def tabulate_votes(submission, thread_id):
     aye = 0
     nay = 0
     abst = 0
-
-
-def tabulate_votes(submission, thread_id):
+    alerts = 0
     print(datetime.datetime.utcnow().isoformat() + " Plugins::Secure // [INFO] Securing Thread " + thread_id + ".")
     capture_message(datetime.datetime.utcnow().isoformat() + " Plugins::Secure // [INFO] Securing Thread " + thread_id + ".")
     submission.comments.replace_more(limit=None)
     all_comments = submission.comments.list()
     comm_count = len(all_comments)
     for i in all_comments:
-        if reddit.parse_vote(i.body) == 1:
-            aye + 1
-        if reddit.parse_vote(i.body) == 0:
-            nay + 1
-        if reddit.parse_vote(i.body) == 2:
-            abst + 1
+        if reddit.parse_vote(i.body, i.author.name) == 1:
+            aye = aye + 1
+        elif reddit.parse_vote(i.body, i.author.name) == 0:
+            nay = nay + 1
+        elif reddit.parse_vote(i.body, i.author.name) == 2:
+            abst = abst + 1
+        elif reddit.parse_vote(i.body, i.author.name) == 10:
+            print(" Plugins::Secure // [INFO] I see myself!")
         else:
             print(datetime.datetime.utcnow().isoformat() + " Plugins::Secure // [WARN] Parser Mismatch (" + i.id + ").")
             capture_message(datetime.datetime.utcnow().isoformat() + " Plugins::Secure // [WARN] Parser Mismatch (" + i.id + ").")
-    count = [aye], [nay], [abst]
+            alerts = alerts + 1
+    count = [aye], [nay], [abst], [alerts]
     return count
-
 
 
 def secure_thread(submission, count):
     aye = count[0]
     nay = count[1]
     abst = count[2]
-    submission.reply("THREAD IS SECURED! \n\n \n\n Ayes: " + str(aye) + " \n\n Nays: " + str(nay) + " \n\n Abstain: " + str(abst))
+    alerts = count[3]
+    if alerts == 0:
+        submission.reply("THREAD IS SECURED! \n\n \n\n Ayes: " + str(aye) + " \n\n Nays: " + str(nay) + " \n\n Abstain: " + str(abst))
+    if alerts != 0:
+        submission.reply(emoji.emojize(":biohazard:") + " I HAVE ALERTS! CHECK SENTRY! " + emoji.emojize(":biohazard:") + " \n\n \n\n \n\n \n\n THREAD IS SECURED WITH " + emoji.emojize(":biohazard:") + " " + str(alerts) + " ALERTS! \n\n \n\n Ayes: " + str(aye) + " \n\n Nays: " + str(nay) + " \n\n Abstain: " + str(abst))
     submission.mod.lock()
